53d1c4d295eb70a9c4c06b38957667e736839f04
Deo-Ashish <deoashish942@gmail.com>
2026-02-09 16:19:21 +0530

    added all files of idea

---
+++ src/CW/sumOfDigits.java
@@ -0,0 +1,7 @@
+public class sumOfDigits {
+    public static void main(String[] args) {
+        int n, sum = 0;
+        System.out.print("Enter any number to find its sum: ");
+        Scanner sc = new Scanner(System.in);
+    }
+}
