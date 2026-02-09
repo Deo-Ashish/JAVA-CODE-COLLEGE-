5c63a40e0bb9ee646861a85f919ae054ac45b1fc
Deo-Ashish <deoashish942@gmail.com>
2026-02-10 01:21:02 +0530

    added all codes

---
+++ CW/factorial.java
@@ -0,0 +1,21 @@
+package CW;
+
+import java.util.Scanner;
+
+public class factorial {
+    static void main(String[] args) {
+        int n, fact = 1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = sc.nextInt();
+        int i = 1;
+
+        while(i <= n){
+            fact = fact * i;
+            i++;
+        }
+
+        System.out.println("factorial = " + fact);
+    }
+}
---
+++ CW/ramanujanNumber.java
@@ -0,0 +1,27 @@
+package CW;
+
+import java.lang.classfile.attribute.SyntheticAttribute;
+import java.util.Scanner;
+
+public class ramanujanNumber {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number to check Ramanujan's number");
+        int n = sc.nextInt();
+
+        int count = 0;
+        for(int i=1; i*i*i<n; i++){
+            for (int j=i+1; j*j*j<n; j++){
+                if (i*i*i + j*j*j == n){
+                    count++;
+                }
+            }
+        }
+        if (count >= 2){
+            System.out.println("This is a Ramanujan's number.");
+        }
+        else {
+            System.out.println("This is not a Ramanujan's number.");
+        }
+    }
+}
