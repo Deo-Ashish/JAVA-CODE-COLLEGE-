d20060493769a00499de344c73e63939789264a7
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-02-13 21:41:06 +0530

    Delete src directory

--- src/CW/sumOfDigits.java
+++
@@ -1,7 +0,0 @@
-public class sumOfDigits {
-    public static void main(String[] args) {
-        int n, sum = 0;
-        System.out.print("Enter any number to find its sum: ");
-        Scanner sc = new Scanner(System.in);
-    }
-}
